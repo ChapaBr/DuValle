3f4821ddfcc6065702169f980c63e888f521cd81
ChapaBr <gui_2512@hotmail.com>
2019-08-17 01:16:32 -0300

    Origin Master

--- envio.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$inputNome = $_POST['inputNome'];
-$inputTelefone = $_POST['inputTelefone'];
-$inputEmail = $_POST['inputEmail'];
-$inputMensagem = $_POST['inputMensagem'];
-
-echo $inputNome.'<br>'.$inputTelefone.'<br>'.$inputEmail.'<br>'.$inputMensagem;
