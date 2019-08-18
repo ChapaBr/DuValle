bbc6373a27c808a9e137740f0ba7591719f95f16
Nathan Dev <nathan@camposwebsolutions.com>
2019-08-18 17:03:22 -0300

    Origin Master

--- enviar.php
+++ enviar.php
@@ -22,25 +22,26 @@ try {
     $mail->isSMTP();                                            // Set mailer to use SMTP
     $mail->Host       = 'smtp.gmail.com';  // Specify main and backup SMTP servers
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'devnathan45@gmail.com';                     // SMTP username
-    $mail->Password   = 'nicolas1408';                               // SMTP password
+    $mail->Username   = 'website@duvalle.agr.br';                     // SMTP username
+    $mail->Password   = 'X3lo84Cq8m';                               // SMTP password
     $mail->SMTPSecure = 'ssl';                                  // Enable TLS encryption, `ssl` also accepted
     $mail->Port       = 465;                                    // TCP port to connect to
 
     //Recipients
-    $mail->setFrom('devnathan45@gmail.com', 'Teste de email'); //aqui vc coloca o email do cliente 
-    $mail->addAddress('devnathan45@gmail.com', 'Joe User');     // Add a recipient
-    $mail->addReplyTo('devnathan45@gmail.com', 'Information');
+    $mail->setFrom('website@duvalle.agr.br', 'Contato - ' . $nome); //aqui vc coloca o email do cliente 
+    $mail->addAddress('website@duvalle.agr.br', 'Duvalle');     // Add a recipient
+    // $mail->addReplyTo('devnathan45@gmail.com', 'Information');
 
     // Content
     $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Teste de envio';
+    $mail->Subject = 'Contato - Home';
     $mail->Body    = "Nome: ".$nome."<br> Telefone: ".$telefone."<br> E-mail: ".$email."<br> Mensagem: ".$mensagem;
 
     $mail->send();
 
     //Aqui você pode dar uma header location
-    echo 'Email enviado com sucesso';
+    header('location: index.html?email=success');
 } catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    header('location: index.html?email=error');
+    // echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
