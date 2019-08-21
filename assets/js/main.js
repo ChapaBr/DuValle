a0dc15f292aa876c2b091c094a208404d52c917f
chapabr <gui_2512@hotmail.com>
2019-08-21 11:21:39 -0300

    Origin Master

--- assets/js/main.js
+++ assets/js/main.js
@@ -194,10 +194,6 @@ $(document).ready(function(){
 			$(this).find('.barraBot').css("bottom", "10px");
 		}
 	});
-	$('.cepMask').mask('00000-000');
-	$('.telMask').mask('(00)0000-0000');
-	$('.cpfMask').mask('000.000.000-00');
-	$('.cnpjMask').mask('00.000.000/0000-00');
 	$('.tipoClick').click(function(){
 		if($("#pessoaFisica").is(":checked")) {
 			$('.btnJuridicaLabel').removeClass('buttonVerde');
